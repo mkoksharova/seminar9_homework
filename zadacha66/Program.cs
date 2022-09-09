79b6f9a240d527ff23096b6856b315f4f64cab7d
mkoksharova <110388813+mkoksharova@users.noreply.github.com>
2022-09-09 11:09:36 +0300

    final save

---
+++ zadacha66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SummOfMN(int m, int n, int summ = 0)
+{
+    //Console.WriteLine($"{i}, {j}, {sum}");
+    summ += m;
+    m++;
+    if (m > n)
+    {
+        Console.WriteLine($"Сумма чисел в  интервале равна : {summ}");
+        return;
+    }
+    SummOfMN(m, n, summ);
+}
+SummOfMN(4, 8);
---
+++ zadacha68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+int Ackermann(int m, int n)
+{
+
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    return Ackermann((m - 1), Ackermann(m, n - 1));
+}
+Console.WriteLine(Ackermann(2, 3));
